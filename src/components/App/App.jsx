28779be8369837c7eb20a649678cf0b90a253d46
Andrey <artist_hire@ukr.net>
2022-06-21 11:20:38 +0300

    Change App component

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import { Wrapper, Container } from './App.styled';
+
+const App = () => {
+  return (
+    <Wrapper>
+      <Container>React homework template</Container>
+    </Wrapper>
+  );
+};
+
+export default App;
