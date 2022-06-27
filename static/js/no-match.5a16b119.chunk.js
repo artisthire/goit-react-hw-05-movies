392cb60f9bf10a83afa2ac3992f042e2720622da
artisthire <artist_hire@ukr.net>
2022-06-27 08:27:25 +0000

    Deploying to gh-pages from @ artisthire/goit-react-hw-05-movies@d870aafd934cf6e7e0070ba5a2e7ee637f0e5a33 🚀

---
+++ static/js/no-match.5a16b119.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[715],{2790:function(e,t,n){n.r(t);var o=n(501),c=n(5903),i=n(184);t.default=function(){return(0,i.jsx)(c.Z,{title:"Page not found",children:(0,i.jsx)(o.rU,{to:"/",children:"Back to homepage"})})}}}]);
+//# sourceMappingURL=no-match.5a16b119.chunk.js.map
